1124c3b385265cd9e683711b5b9588627c9eb5d4
Josh Curl <josh@curl.me>
2019-11-14 14:34:21 -0800

    Device API -> Device Variables

--- components/docs.js
+++ components/docs.js
@@ -38,8 +38,8 @@ const list = [
     nested: true
   },
   {
-    href: '/docs/device-api',
-    title: 'Device API'
+    href: '/docs/device-variables',
+    title: 'Device Variables'
   },
   {
     href: '/docs/cli',
