84c51ac82ec4cd77bf1ff519074f498158e9df71
raboid <rabautse@gmail.com>
2020-01-28 09:24:34 -0800

    update third party scripts

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,7 @@ const Intercom = () => (
       }}
     />
     <script
+      defer
       dangerouslySetInnerHTML={{
         __html: `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/' + '${INTERCOM_ID}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();`
       }}
@@ -30,6 +31,7 @@ const Intercom = () => (
 
 const Segment = () => (
   <script
+    defer
     dangerouslySetInnerHTML={{
       __html: `
     !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
@@ -111,12 +113,6 @@ class Document extends NextDocument {
             href="https://fonts.googleapis.com/css?family=Rubik:300,400,500&display=swap"
             rel="stylesheet"
           />
-        </Head>
-        <body>
-          <main>
-            <Main />
-          </main>
-          <NextScript />
 
           {process.env['NODE_ENV'] === 'development' ? null : (
             <>
@@ -124,6 +120,12 @@ class Document extends NextDocument {
               <Segment />
             </>
           )}
+        </Head>
+        <body>
+          <main>
+            <Main />
+          </main>
+          <NextScript />
         </body>
       </Html>
     );
