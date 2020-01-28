070023f00aaacf457f423c00ef76e7099582fe13
raboid <rabautse@gmail.com>
2020-01-28 09:20:27 -0800

    add ga preconnect

--- pages/_document.js
+++ pages/_document.js
@@ -102,6 +102,12 @@ class Document extends NextDocument {
             crossOrigin="true"
           />
           <link
+            rel="preconnect"
+            href="https://www.google-analytics.com"
+            crossOrigin="true"
+          />
+
+          <link
             href="https://fonts.googleapis.com/css?family=Rubik:300,400,500&display=swap"
             rel="stylesheet"
           />
